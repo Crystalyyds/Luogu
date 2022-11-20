bd80db4bdfdb31fced88734b0fed4ee5851b0583
YangJiLei <2031291160@qq.com>
2022-11-20 11:21:03 +0800

    set

--- ACWing/BasicAlgorithm/two/DoublePointers/799/Main.cpp
+++ ACWing/BasicAlgorithm/two/DoublePointers/799/Main.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, arr[100010], brr[100010], cnt;
-int main() {
+int main()
+{
   cin >> n;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1; i <= n; i++)
+  {
     cin >> arr[i];
   }
   cnt = 0;
-  for (int i = 1, j = 1; i <= n; i++) {
+  for (int i = 1, j = 1; i <= n; i++)
+  {
     brr[arr[i]] += 1;
-    while (brr[arr[i]] > 1) {
+    while (brr[arr[i]] > 1)
+    {
       brr[arr[j]]--;
       j++;
     }
--- ACWing/BasicAlgorithm/two/heap/836/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int N = 100010;
-int n, m;
-int main() {
-  //   scanf("%d%d", &n, &m);
-  cout << "的" << endl;
-  return 0;
-}
